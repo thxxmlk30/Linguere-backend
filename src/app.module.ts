e8937e3777bd775d85070b704bd76ecfecfb7b4e
malick <malicksene622@gmail.com>
2026-07-03 10:16:12 +0000

    configuration de Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { buildTypeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // rend le .env accessible partout
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: buildTypeOrmConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
