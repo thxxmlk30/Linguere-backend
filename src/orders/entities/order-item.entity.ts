e284da595af7fa46cc9446411010e0c6a455f0a2
malick <malicksene622@gmail.com>
2026-07-03 14:40:00 +0000

    Connfiguration de l'entité pour le module orders

---
+++ src/orders/entities/order-item.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Order } from './order.entity';
+import { MenuItem } from '../../menu/entities/menu-item.entity';
+
+@Entity('order_items')
+export class OrderItem {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Order, (order) => order.items, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'orderId' })
+  order: Order;
+
+  @Column()
+  orderId: string;
+
+  @ManyToOne(() => MenuItem, { eager: true })
+  @JoinColumn({ name: 'menuItemId' })
+  menuItem: MenuItem;
+
+  @Column()
+  menuItemId: string;
+
+  @Column({ type: 'int' })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  unitPrice: number;
+}
---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { OrderItem } from './order-item.entity';
+import { OrderStatus } from '../../common/enums/order-status.enum';
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, (user) => user.orders, { eager: true })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  userId: string;
+
+  @OneToMany(() => OrderItem, (item) => item.order, {
+    cascade: true,
+    eager: true,
+  })
+  items: OrderItem[];
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  totalAmount: number;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
+  status: OrderStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
